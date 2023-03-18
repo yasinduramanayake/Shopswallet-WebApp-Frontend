b83292fc0b3789418d893b07f3a98dc92af661ca
yasinduyuga <yasindu@smartedesigners.com>
2023-03-18 21:36:03 +0530

    updated

--- src/views/BecomeASeller.vue
+++ src/views/BecomeASeller.vue
@@ -3,14 +3,14 @@
 		<div class="container">
 			<div class="text-center text-white" style="padding-top: 180px;">
 				<div class="fs-1">
-					Everything you need to start selling online!
+					{{ $t("BecomeAseller.Header") }}
 				</div>
 				<div class="fs-5 text-uppercase text-bold mb-3">
-					It's Made for You
+					{{ $t("BecomeAseller.SubHeader") }}
 				</div>
 				<div>
 					<a href="#" class="btn btn-warning rounded-0 text-lead text-white text-uppercase">
-						Start Selling
+						{{ $t("BecomeAseller.Button") }}
 					</a>
 				</div>
 			</div>
@@ -22,29 +22,29 @@
 				<div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 					<ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
   						<li class="nav-item" role="presentation">
-    						<button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Benefits</button>
+    						<button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">{{ $t("BecomeAseller.Tab1Header") }}</button>
   						</li>
   						<li class="nav-item" role="presentation">
-    						<button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">How It Works</button>
+    						<button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">{{ $t("BecomeAseller.Tab2Header") }}</button>
   						</li>
   						<li class="nav-item" role="presentation">
-    						<button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">FAQ</button>
+    						<button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">{{ $t("BecomeAseller.Tab3Header") }}</button>
   						</li>
 					</ul>
 					<div class="tab-content" id="myTabContent">
   						<div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-  							<h2 class="text-center text-uppercase mt-4">Benefits</h2>
-  							<div class="text-center text-muted fst-italic mb-5">Selling online has never been easier.</div>
+  							<h2 class="text-center text-uppercase mt-4">{{ $t("BecomeAseller.Tab1ContentHeader") }}</h2>
+  							<div class="text-center text-muted fst-italic mb-5">{{ $t("BecomeAseller.Tab1ContentSubHeader")}}</div>
   							<div class="row text-center">
   								<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
   									<div class="rounded-circle bg-dark d-flex justify-content-center align-content-center flex-wrap mb-3 mx-auto" style="width: 100px; height: 100px; font-size: 50px;">
   										<font-awesome-icon icon="fa-solid fa-rocket" class="text-white" />
   									</div>
 
-  									<h5>Start Selling in Minutes</h5>
+  									<h5>{{ $t("BecomeAseller.Tab1ContentRow1Header")}}</h5>
 
   									<div class="text-muted">
-  										Get straight to growing your business. We handle everything to sell online. So you can focus on your business rather managing the infrastructure.
+										{{ $t("BecomeAseller.Tab1ContentRow1Content")}}	
   									</div>
   								</div>
   								<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
@@ -52,10 +52,10 @@
   										<font-awesome-icon icon="fa-solid fa-tablet-screen-button" class="text-white" />
   									</div>
 
-  									<h5>Comprehensive Dashboard</h5>
+  									<h5>{{ $t("BecomeAseller.Tab1ContentRow2Header")}}</h5>
 
   									<div class="text-muted">
-  										Get a holistic and detailed view of your business to better understand sales, orders, and customers to better tailor your products.
+										{{ $t("BecomeAseller.Tab1ContentRow2Content")}} 
   									</div>
   								</div>
   								<div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
@@ -63,16 +63,16 @@
   										<font-awesome-icon icon="fa-solid fa-credit-card" class="text-white" />
   									</div>
 
-  									<h5>Get Paid Directly and Securely</h5>
+  									<h5> 	{{ $t("BecomeAseller.Tab1ContentRow3Header ")}}</h5>
 
   									<div class="text-muted">
-  										We don't keep your money! The payment goes to you directly as soon as a buyer pays.
+										{{ $t("BecomeAseller.Tab1ContentRow3Content")}}	
   									</div>
   								</div>
   							</div>
   						</div>
-  						<div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">How It Works</div>
-  						<div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">FAQ</div>
+  						<div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">	{{ $t("BecomeAseller.Tab2Header")}}	</div>
+  						<div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">	{{ $t("BecomeAseller.Tab3Header")}}</div>
 					</div>
 				</div>
 			</div>
